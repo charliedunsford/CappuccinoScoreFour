31b54f7fcd6fe339f920d220c38551d7b9ca7b4a
charliedunsford <184438218+charliedunsford@users.noreply.github.com>
2025-02-18 23:47:39 -0800

    Implementing basic game mode and UI functions`
    Implemented game mode selection via args
    
    Implemented GUI on its own thread

---
+++ src/GamePanel.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    private Thread panelThread;
+    private final GameBoard gameBoard;
+
+    public GamePanel() {
+        this.gameBoard = new GameBoard();
+        this.setPreferredSize(new Dimension(800,500));
+    }
+
+    public void startPanelThread() {
+        panelThread = new Thread(this);
+        panelThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        int FPS = 60;
+        double drawInterval =  (double) 1000000000 / FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (panelThread != null) {
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+        gameBoard.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        gameBoard.draw(g2d);
+
+        g2d.dispose();
+    }
+}
---
+++ src/GameState.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class GameState {
+
+    // Sets up the game for a normal runtime
+    public void setupNormalMode() {
+        startGui();
+    }
+
+    // Sets up game for a debugging runtime
+    public void setupTestMode() {
+        // Do not run GUI
+    }
+
+    public void startGame() {
+        // Game loop would go here
+    }
+
+    // Starts GUI on its own thread
+    public void startGui() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Cappuccino Score Four");
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        panel.startPanelThread();
+    }
+}
