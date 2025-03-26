34dfa31c25bac5b7cbcc0d993c43054693eec56f
Luke-Smyth45 <lukesmyth223@gmail.com>
2025-03-26 11:34:37 -0700

    a random AI that will only play valid moves
    a random AI that will only play valid moves, a smarter AI is in development

--- src/scorefour/player/ComputerPlayer.java
+++ src/scorefour/player/ComputerPlayer.java
@@ -1,21 +1,47 @@
 package scorefour.player;
 
 import scorefour.common.BeadColour;
-
+import scorefour.model.Board;
+import scorefour.model.Peg;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ComputerPlayer extends Player {
 
-    public ComputerPlayer(BeadColour colour) {
+    Board board = new Board();
+    private Peg[][] pegs;
+
+    public ComputerPlayer(BeadColour colour)
+    {
         super(colour);
+        pegs = board.getPegs();
     }
 
     public int[] getMove() {
         Random rand = new Random();
 
-        int row = rand.nextInt(4);
-        int col = rand.nextInt(4);
+        ArrayList<int[]> ValidMoves = getValidMoves();
+        int randomPlace = rand.nextInt(ValidMoves.size());
+        return ValidMoves.get(randomPlace);
 
-        return new int[]{row, col};
     }
+
+    public ArrayList getValidMoves ()
+    {
+        ArrayList<int[]> ValidMoves = new ArrayList<int[]>();
+        for (int r=0; r<4; r++)
+        {
+            for(int c=0; c<4; c++)
+            {
+                int[] position = new int[]{r,c};
+                if(pegs[r][c].getBeads()[3]==null)
+                {
+                    ValidMoves.add(position);
+                }
+
+            }
+        }
+        return  ValidMoves;
+    }
+
 }
